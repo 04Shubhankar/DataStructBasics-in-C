0f0ed990ecbd82c3eae63d85b5b8eef8096e6312
cosine2A <109309785+cosine2A@users.noreply.github.com>
2024-08-25 00:06:54 +0530

    Create CreateLinkedList.c
    This C program effectively creates a linked list data structure. It allows users to specify the number of nodes and enter data for each node. The program then displays the created linked list.

---
+++ CreateLinkedList.c
@@ -0,0 +1,44 @@
+// Creation of a Linked List 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+} *first , *last , *current;
+
+int main(){
+    int num;
+    printf("Enter the number of nodes to create ");
+    scanf("%d",&num);
+
+    // Create first Node 
+    first = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data for the first node ");
+    scanf("%d",&first->data);
+    first->next = NULL;
+    last = first;
+
+    // Create remaining nodes
+    for(int i=1; i<num; i++){
+        current = (struct node*)malloc(sizeof(struct node));
+        printf("Enter the data for the %d node ",i+1);
+        scanf("%d",&current->data);
+        current->next = NULL;
+        last->next = current;
+        last = current;
+    }
+
+    // Display the Linked List
+    printf("\nLinked List: ");
+    current = first;
+    while(current!= NULL){
+        printf("%d -> ",current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+
+
+
+}
